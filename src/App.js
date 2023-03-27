d4cc9b4482fe107d6b0594dc6f4779124ed1ad94
Douglas Lima <douglas07a@gmail.com>
2023-03-27 12:17:02 -0300

    Função Carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-
 import './App.css';
-import './Json.js';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Carousel from 'react-bootstrap/Carousel';
 
 
 function App() {
@@ -20,7 +21,15 @@ function App() {
 
         <div className='praia'>
           <h2>Praia</h2>
-          <img src='./assets/img/praia1.jpg'/>
+          <Carousel fade>
+            <Carousel.Item>
+          <img  src='./assets/img/praia1.jpg'/>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img  src='./assets/img/praia2.jpg'/>
+            </Carousel.Item>
+            </Carousel>
+          
           <p>
             Já imaginou curtir a sombra dos coqueiros enquanto recebe raios dourados de sol e se refresca no mar 
             quentinho de ondas calmas? Não pense mais, vá curtir o litoral agora!
@@ -30,22 +39,33 @@ function App() {
 
         <div className='cruzeiro'>
           <h2>Cruzeiro</h2>
+          <Carousel fade>
+            <Carousel.Item>
           <img src='./assets/img/cruzeiros1.jpg'/>
-          
-          
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src='./assets/img/cruzeiros2.jpg'/>
+          </Carousel.Item>
+          </Carousel>
           <p>
             Em uma viagem de navio, basta entrar, desfazer as malas e relaxar!
             Você só precisa escolher o destino e o tipo de cabine. Durante a viagem os navios têm
             programação para todos os gostos e idades a todo o tempo. Têm espaços para crianças se
             divertirem, enquanto você poderá relaxar em um SPA ou até mesmo, tentar a sorte no cassino $$$.
           </p>
-
+          
         </div>
 
         <div className='resort'>
           <h2>Resort</h2>
+          <Carousel fade>
+            <Carousel.Item>
           <img src='./assets/img/resort1.jpg'/>
-          
+          </Carousel.Item>
+          <Carousel.Item>
+            <img src='./assets/img/resort2.jpg'/>
+          </Carousel.Item>
+          </Carousel>
           <p>
             Viajar para um resort é uma forma de aproveitar a natureza, existem opções em praias, fazendas e montanhas.
             Sem abdicar do conforto de um excelente hotel.
--- src/componente.js
+++
@@ -1,6 +0,0 @@
-function viagens ({destino,img}) {
-    return (
-        <h1>{destino}</h1>
-        <img src={img}/>
-    )
-}
